9f82f4dd18bea94977ab5c444d5a9d031a405490
Natalia Gavrilenko <natalia.gavrilenko@aalto.fi>
2020-01-04 17:56:48 +0200

    UnInit event

---
+++ dartagnan/src/main/java/com/dat3m/dartagnan/program/event/UnInit.java
@@ -0,0 +1,43 @@
+package com.dat3m.dartagnan.program.event;
+
+import com.dat3m.dartagnan.expression.ExprInterface;
+import com.dat3m.dartagnan.program.utils.EType;
+
+public class UnInit extends MemEvent {
+
+    private final MemEvent event;
+
+    public UnInit(MemEvent e){
+        super(e.getAddress(), null);
+        this.event = e;
+        addFilters(EType.ANY, EType.VISIBLE, EType.MEMORY, EType.WRITE, EType.UNINIT);
+    }
+
+    private UnInit(UnInit other){
+        super(other);
+        this.event = other.event;
+    }
+
+    @Override
+    public String toString() {
+        return "*" + address + " := N/A";
+    }
+
+    @Override
+    public String label(){
+        return "UW";
+    }
+
+    @Override
+    public ExprInterface getMemValue(){
+        return event.getMemValue();
+    }
+
+    // Unrolling
+    // -----------------------------------------------------------------------------------------------------------------
+
+    @Override
+    public UnInit getCopy(){
+        return new UnInit(this);
+    }
+}
